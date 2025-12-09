66e186b01e4007748803eb99ee79ffb6a4ceeac6
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-09 11:16:45 +0100

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-REGION="us-east-1"
 SG_ID="sg-093dd9ce6f294bb69"
 
 for instance in $@; 
@@ -12,28 +11,20 @@ do
         --security-group-ids $SG_ID \
         --region $REGION \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --count 1 \
         --query 'Instances[0].InstanceId' \
         --output text)
     
     #Get public  and Private IPs
     if [ $instance != "frontend" ]; then
-        echo "Waiting for 2 mins for instance to be in running state"
-        sleep 10
-        Private_IP=$(aws ec2 describe-instances \
+        IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --region $REGION \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text)
-        echo "$instance Private IP: $Private_IP"
     else
-        echo "Waiting for 2 mins for instance to be in running state"
-        sleep 10
-        Public_IP=$(aws ec2 describe-instances \
+        IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --region $REGION \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text)
-        echo "$instance Public IP: $Public_IP"
     fi
+    echo "$instance IP: $IP"
 done
